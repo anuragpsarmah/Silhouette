8302bee9309a31e89582d9817b351f6eb3f90148
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-06-13 17:08:30 +0530

    Update messageSchema to make imageUrl optional + defined sendMessage route

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 
 export const messageSchema = z.object({
+  username: z.string().min(3, { message: "Username must be atleast 3 characters long" }),
   content: z
     .string()
     .min(10, { message: "Message must be atleast 10 characters long" })
     .max(300, { message: "Message must be atmost 300 charaters long" }),
-  imageUrl: z.string().url(),
+  imageUrl: z.string().url().optional(),
   createdAt: z.date(),
 });
