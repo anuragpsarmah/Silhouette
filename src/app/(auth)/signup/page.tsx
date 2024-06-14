3f04057b3f1b77f7be01b58040381371d5274d46
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-06-14 08:03:57 +0530

    Created signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,98 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link"; // Import the Link component
+import { HeroHighlight } from "@/components/ui/hero-highlight";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import Zoom from "@mui/material/Zoom";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-export default function page() {
+export default function Page() {
+  const [zoomIn, setZoomIn] = useState<boolean>(true);
+
   return (
-    <div>page</div>
-  )
+    <HeroHighlight>
+      <center>
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+            },
+            {
+              name: "Contact",
+              link: "https://www.anuragpsarmah.me/#contact",
+            },
+          ]}
+        />
+      </center>
+      <Zoom in={zoomIn} timeout={220}>
+        <form className="max-w-md mx-auto mt-[6rem] space-y-4 p-6 bg-black text-white border border-gray-500 rounded-lg shadow-lg lg:w-[34rem] mt-5">
+          <h2 className="text-2xl font-bold text-center">Sign Up</h2>
+
+          <div className="space-y-1">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+              className="bg-black text-white placeholder-neutral-400"
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="bg-black text-white placeholder-neutral-400"
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              className="bg-black text-white placeholder-neutral-400"
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label htmlFor="image">Profile Image</Label>
+            <Input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              className="bg-black text-white"
+              style={{ color: "rgb(163 163 163)" }}
+            />
+          </div>
+
+          <center>
+            <button
+              type="button"
+              className="text-white inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            >
+              Sign Up
+            </button>
+          </center>
+          <div className="text-center text-sm mt-4">
+            <p>
+              Already have an account?{" "}
+              <Link href="/signin">
+                <span style={{color: "rgb(51 107 198)"}}>Sign in</span>
+              </Link>
+            </p>
+          </div>
+        </form>
+      </Zoom>
+    </HeroHighlight>
+  );
 }
--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -51,10 +51,11 @@ export const FloatingNav = ({
               <span className="hidden sm:block text-sm">{navItem.name}</span>
             </Link>
           ))}
+          <Link href={"/signup"}>
           <button className="register border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
             <span>Register</span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
-          </button>
+          </button></Link>
         </motion.div>
       )}
     </AnimatePresence>
