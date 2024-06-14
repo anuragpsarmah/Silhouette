f920fdc0fa13a407a95e28d8ebd6038300857147
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-06-14 08:18:49 +0530

    Responsiveness fixes

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -35,7 +35,7 @@ export const FloatingNav = ({
             duration: 0.2,
           }}
           className={cn(
-            "container flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-4 py-2 items-center justify-center space-x-4",
+            "container flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 lg:pl-4 md:pl-4 sm:pl-4 py-2 items-center justify-center space-x-4",
             className
           )}
         >
